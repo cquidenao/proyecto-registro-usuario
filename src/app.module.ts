cfc59098dd32ac8a79f69973cf5777647b0136f7
cquidenao <c.a.p.quidenao@gmail.com>
2024-06-22 20:10:31 -0400

    configuracin basica

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsuarioModule } from './usuario/usuario.module';
+import { Usuario } from './usuario/usuario.entity';
 
 @Module({
-  imports: [UsuarioModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql', // o el tipo de base de datos que estés usando
+      host: 'localhost',
+      port: 3306,
+      username: 'tu_usuario',
+      password: 'tu_contraseña',
+      database: 'tu_base_de_datos',
+      entities: [Usuario],
+      synchronize: true,
+    })
+    UsuarioModule,
+  ]
 })
 export class AppModule {}
